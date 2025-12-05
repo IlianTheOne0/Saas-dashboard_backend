8939d5380233e928465f512effd62fc1a1be2ccc
Ilian Shchepinskyi <iliantheone@gmail.com>
2025-12-06 01:30:45 +0200

    Added work with correlation id for data protection

--- Service/Worker.cs
+++ Service/Worker.cs
@@ -10,25 +10,23 @@ public partial class Worker : Base
 {
     private const string TAG = "SERVICE-WORKER";
 
-    private readonly Dictionary<string, Func<JsonElement, Task<string>>>? _handlers = null;
+    private readonly Dictionary<string, Func<JsonElement, string, Task<string>>>? _handlers = null;
 
     public Worker()
     {
         Logger.Debug(TAG, "Registering Handlers...");
-
-        _handlers = new Dictionary<string, Func<JsonElement, Task<string>>>
+        
+        _handlers = new Dictionary<string, Func<JsonElement, string, Task<string>>>
         {
-            { "register", data => _ForwardToDatabase("register", data) },
-            { "login", data => _ForwardToDatabase("login", data) },
-            { "recovery_password", data => _ForwardToDatabase("recovery_password", data) },
-            { "new_password", data => _ForwardToDatabase("new_password", data) },
+            { "register", (data, cid) => _ForwardToDatabase("register", cid, data) },
+            { "login", (data, cid) => _ForwardToDatabase("login", cid, data) },
+            { "recovery_password", (data, cid) => _ForwardToDatabase("recovery_password", cid, data) },
+            { "new_password", (data, cid) => _ForwardToDatabase("new_password", cid, data) },
 
-            { "register-answer", data => _ForwardToFrontend("register-answer", data) },
-            { "login-answer", data => _ForwardToFrontend("login-answer", data) },
-            { "recovery_password-answer", data => _ForwardToFrontend("recovery_password-answer", data) },
-            { "new_password-answer", data => _ForwardToFrontend("new_password-answer", data) },
-
-            { "error", data => _ForwardToFrontend("error", data) }
+            { "register-answer", (data, cid) => _ForwardToFrontend("register-answer", cid, data) },
+            { "login-answer", (data, cid) => _ForwardToFrontend("login-answer", cid, data) },
+            { "recovery_password-answer", (data, cid) => _ForwardToFrontend("recovery_password-answer", cid, data) },
+            { "new_password-answer", (data, cid) => _ForwardToFrontend("new_password-answer", cid, data) },
         };
 
         Logger.Info(TAG, $"Handlers registered: {_handlers.Count}");
@@ -46,19 +44,23 @@ public partial class Worker
 
     protected override async Task<string> ProcessMessage(string message)
     {
+        string currentCorrelationId = "";
+
         try
         {
             var unit = JsonSerializer.Deserialize<MUnit>(message);
             if (unit == null || string.IsNullOrWhiteSpace(unit.Event)) { Logger.Warn(TAG, "Invalid message format received."); throw new Exception("Invalid message format"); }
 
-            Logger.Info(TAG, $"Routing to handler for event: '{unit.Event}'");
+            currentCorrelationId = unit.CorrelationId;
+
+            Logger.Info(TAG, $"Routing '{unit.Event}' with such correlation id: '{currentCorrelationId}'");
 
             if (_handlers == null) { throw new Exception("No handlers are registered"); }
 
             if (_handlers.TryGetValue(unit.Event, out var handler))
             {
                 Logger.Debug(TAG, "Handler found. Executing...");
-                var result = await handler(unit.Data);
+                var result = await handler(unit.Data, currentCorrelationId);
                 Logger.Debug(TAG, "Handler execution complete.");
                 return result;
             }
@@ -73,11 +75,11 @@ public partial class Worker
 
 public partial class Worker
 {
-    private async Task<string> _ForwardToDatabase(string eventName, JsonElement data)
+    private async Task<string> _ForwardToDatabase(string eventName, string cid, JsonElement data)
     {
         Logger.Info(TAG, $"[ROUTING] Forwarding '{eventName}' to Database...");
 
-        var wrapper = new MUnit() { Event = eventName, Data = data };
+        var wrapper = new MUnit() { Event = eventName, CorrelationId = cid, Data = data };
         string payload = JsonSerializer.Serialize(wrapper);
 
         await produceMessage("database-topic", payload);
@@ -85,11 +87,11 @@ public partial class Worker
         return String.Empty;
     }
 
-    private async Task<string> _ForwardToFrontend(string eventName, JsonElement data)
+    private async Task<string> _ForwardToFrontend(string eventName, string cid, JsonElement data)
     {
         Logger.Info(TAG, $"[ROUTING] Forwarding '{eventName}' to Frontend...");
 
-        var wrapper = new MUnit() { Event = eventName, Data = data };
+        var wrapper = new MUnit() { Event = eventName, CorrelationId = cid, Data = data };
         string payload = JsonSerializer.Serialize(wrapper);
 
         await produceMessage("auth-topic-answers", payload);
